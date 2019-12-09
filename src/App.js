9f51299dfeb40b3167adbb426b621344701d2a4a
1stmuse <olysegs@gmail.com>
2019-12-09 02:16:59 -0500

    created the logo, rank and imageLink component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import Navigation from './components/navigation/Navigation'
 import Logo from './components/logo/Logo'
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm'
+import Rank from './components/rank/Rank'
+import FaceRecognition from './components/faceRecognition/FaceRecognition'
 import './App.css';
 
 class App extends React.Component {
 
+  constructor(){
+    super();
+    this.state={
+      input:'',
+      imageUrl:''
+    }
+  }
 
+  onInputChange=(event)=>{
+    // console.log(event.target.value)
+    this.setState({
+      input:event.target.value
+    })
+
+  }
+
+  onSubmit=()=>{
+    this.setState({
+      imageUrl: this.state.input
+    })
+    console.log('submit')
+  }
 
   render(){
     return (
       <div className="App">
         <Navigation/>
-         <Logo/>
-         {/*
-        <ImageLinkForm/>
-        <FaceRecognition/> */}
+        <Logo/>
+        <Rank/>
+        <ImageLinkForm 
+        onSubmit={this.onSubmit}
+        onInputChange={this.onInputChange}/>
+
+        <FaceRecognition imageUrl={this.state.imageUrl} /> 
         
       </div>
     );
---
+++ src/components/faceRecognition/FaceRecognition.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const FaceRecognition = ({imageUrl}) => {
+    return (
+        <div className='center na'>
+            <div className='absolute mt2'>
+            <img src={imageUrl} alt='image'
+            width='500px' height='auto' />
+            </div>
+        </div>
+    );
+};
+
+export default FaceRecognition;
