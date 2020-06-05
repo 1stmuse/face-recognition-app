8ecacd0210188c223e4576feef8e689f18dc5399
1stmuse <olysegs@gmail.com>
2020-06-05 00:56:44 -0400

    added upload succesfully

--- src/components/imageLinkForm/ImageLinkForm.js
+++ src/components/imageLinkForm/ImageLinkForm.js
@@ -4,10 +4,6 @@ import './imagelink.css'
 
 const ImageLinkForm = ({pload, onSubmit}) => {
 const [load, setLoad]=useState(false)
-    let loade;
-    // const image=(e)=>{
-    //     const pictureUrl = e.target.files[0]
-    // }
     const  upload =async(e)=>{
         const files = e.target.files
         const data = new FormData()
@@ -23,9 +19,6 @@ const [load, setLoad]=useState(false)
         const picture = await res.json()
         pload(picture.url)
         setLoad(false)
-        // setTimeout(()=>{
-        //     console.log(files[0])
-        // },5000)
     }
 
     return (
@@ -37,7 +30,7 @@ const [load, setLoad]=useState(false)
                 <div className='center pa4 br3 shadow-2'>
                 {load ? <h1>Loading ...</h1> : 
                 <div className='f4 pa2 w-50 ph5 bg-white' style={{marginRight:'5%'}} >
-                    <input type='file' id='files' ref={(upl)=>{loade = upl}} style={{display:'none'}} onChange={upload} />
+                    <input type='file' id='files' style={{display:'none'}} onChange={upload} />
                     <label htmlFor='files' className='f4 pa2 w-50 center' > upload</label>
                 </div>}
                 <button 
